8fc5a52a0793247a1fc9175495667cced8bc47b9
Bo Wang <wangbostc@hotmail.com>
2024-08-06 00:45:45 +1000

    put user-agent to .env

--- adviser/config.py
+++ adviser/config.py
@@ -4,4 +4,3 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
