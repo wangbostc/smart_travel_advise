328e479ca141b53f7ebea2efab841b2a18b5d32d
Bo Wang <wangbostc@hotmail.com>
2024-08-08 00:02:04 +1000

    1. moved extract_content_from_text to utils 2. change BStransformer to HTML2TEXT so body part will be captured 3.create a function to contruct chain for url retrieve 4. create another chain which takes url to return doc 5.refactored  get_url_for_travel_advice function 6. modifed description in tooling and url retrieve prompt to improve performance

--- adviser/smart_traveler_info_retriver.py
+++ adviser/smart_traveler_info_retriver.py
@@ -1,15 +1,14 @@
-from typing import List, Union, Optional
+from typing import List, Dict
 
 from pydantic import BaseModel, Field
-
-from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
 from langchain_core.tools import tool, StructuredTool
-from langchain_core.language_models import BaseChatModel
-from langchain_core.runnables.base import RunnableBinding, RunnableSequence
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.language_models import BaseChatModel
+from langchain_core.runnables import RunnableBinding, RunnableSequence, RunnableLambda
+from langchain_core.output_parsers.openai_tools import JsonOutputKeyToolsParser
 from langchain_core.documents.base import Document
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.document_transformers import BeautifulSoupTransformer
+from langchain_community.document_transformers import Html2TextTransformer
 
 
 class AdviceInput(BaseModel):
@@ -17,7 +16,7 @@ class AdviceInput(BaseModel):
         description="""
     Lower case continent name that the search country resides in.
     it should be in one of the following 6 regions:
-    africa, americas, asia, europe, middle-east, pacific
+    [africa, americas, asia, europe, middle-east, pacific]
     """
     )
     country: str = Field(
@@ -34,39 +33,75 @@ class AdviceInput(BaseModel):
 
 @tool(args_schema=AdviceInput)
 def advice_whether_safe_to_travel(region: str, country: str) -> str:
-    """Check if it is safe to travel to the country of the region"""
+    """Check if it is safe to travel to the country of the region
+    Sometimes the user may ask about a specific sub-region of the country.
+    If a match of a sub-region is found, find the country of that sub-region first
+    then find the region that country belongs to."""
     return "Exercise normal safety precautions"
 
 
-def get_function_calling_model(
+def get_tool_calling_model(
     chat_model: BaseChatModel,
-    calling_functions: List[StructuredTool],
+    calling_tools: List[StructuredTool],
 ) -> RunnableBinding:
+    """Bind the calling functions to the chat model, here the model is focused on function calling"""
     if not hasattr(chat_model, "bind_functions"):
         raise ValueError("The provide chat model does not support binding functions. ")
-    return chat_model.bind_functions(functions=calling_functions)
+    return chat_model.bind_tools(tools=calling_tools, tool_choice="required")
+
+
+def get_url_for_travel_advice(loc_dict: Dict[str, str]) -> str:
+    """Get the url for travel advice"""
+    region = loc_dict.get("region", "").lower()
+    country = loc_dict.get("country", "").lower()
+    if (not region) | (not country):
+        raise ValueError("Please provide the region and country for travel advice")
+    return f"https://www.smartraveller.gov.au/destinations/{region}/{country}"
 
 
 def construct_chain_for_getting_advice_url(
-    chatmodel: RunnableBinding,
+    chat_model: RunnableBinding,
+    calling_tool: StructuredTool,
 ) -> RunnableSequence:
+    """Construct the chain for getting the advice url
+    Leverage chat model tool calling to retrieve the advice url
+
+    Args:
+        chat_model (RunnableBinding): the chat_model to
+        calling_tool (StructuredTool): _description_
+
+    Returns:
+        RunnableSequence: _description_
+    """
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                "You are a trip advisor which informs whether it is safe to travel to certain country",
+                """You are a trip advisor which informs whether it is safe to travel to a certain country.
+                You have the knowledge of which country the user is asking and which region of this six regions:
+                [africa, americas, asia, europe, middle-east, pacific] the country resides in.
+                Be careful, Indonesia belongs to the asia region, not pacific.
+                """,
             ),
             ("user", "{query}"),
         ]
     )
+    chat_model_with_tool = get_tool_calling_model(
+        chat_model=chat_model, calling_tools=[calling_tool]
+    )
 
-    chain = prompt | chatmodel | OpenAIFunctionsAgentOutputParser()
+    output_parser = JsonOutputKeyToolsParser(key_name=calling_tool.name)
+    # here jsonoutputkeytoolsparsers will return a list of dict, we only need the first one
+
+    chain = (
+        prompt
+        | chat_model_with_tool
+        | output_parser
+        | RunnableLambda(lambda x: x[0])
+        | RunnableLambda(get_url_for_travel_advice)
+    )
+
     return chain
-
-
-def get_url_for_travel_advice(region: str, country: str) -> str:
-    """Get the url for travel advice"""
-    return f"https://www.smartraveller.gov.au/destinations/{region}/{country}"
 
 
 def load_from_url(url: str) -> Document:
@@ -79,52 +114,13 @@ def load_from_url(url: str) -> Document:
 
 
 def transform_html_content(html_content: Document) -> Document:
-    transformer = BeautifulSoupTransformer()
-    docs_transformed = transformer.transform_documents(
-        html_content, tags_to_extract=["p", "li", "div", "a"]
-    )
+    html2text = Html2TextTransformer()
+    docs_transformed = html2text.transform_documents(html_content)
     return docs_transformed[0]
 
 
-def get_smart_traveler_doc_for_advice(
-    user_query: str,
-    chat_model: RunnableBinding,
-    calling_functions: List[StructuredTool],
-) -> str:
-    """Get the advice for travel"""
+def construct_chain_for_advice_doc_from_url() -> Document:
+    """Get the advice information for model to giving user travel advice"""
 
-    chat_model_with_tool = get_function_calling_model(
-        chat_model=chat_model, calling_functions=calling_functions
-    )
-    chain = construct_chain_for_getting_advice_url(chat_model_with_tool)
-    result = chain.invoke({"query": user_query})
-    url = get_url_for_travel_advice(**result.tool_input)
-    smart_traverl_doc_for_advice = load_from_url(url)
-    return smart_traverl_doc_for_advice
-
-
-def extract_content_from_text(
-    text: str,
-    start_word: str,
-    end_word: Optional[str] = "",
-    extraction_length: int = 1000,
-) -> str:
-    """Extract the content from the smart traveler document starts with
-    the search word with lenghth of extraction_length from text. Be careful
-    that the search_word should be case sensitive, and only the first
-    match will be returned.
-    """
-    start_position = text.find(start_word)
-
-    if start_position == -1:
-        return ""
-
-    if end_word == "":
-        return text[start_position : start_position + extraction_length]
-
-    end_position = text.find(end_word, start_position)
-
-    if end_position == -1:
-        end_position = start_position + extraction_length
-
-    return text[start_position:end_position]
+    chain = RunnableLambda(load_from_url) | RunnableLambda(transform_html_content)
+    return chain
