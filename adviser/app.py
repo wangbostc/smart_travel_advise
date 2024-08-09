561ec9e3cd2d21bd99b56df5029fb001bc7fdeb8
Bo Wang <wangbostc@hotmail.com>
2024-08-09 23:24:37 +1000

    removed post api_key method as it is unsecure

--- adviser/app.py
+++ adviser/app.py
@@ -6,15 +6,13 @@ from adviser.advise_model import construct_query2advice_chain
 from adviser.utils import detect_injection
 
 
+class AppQuery(BaseModel):
+    query: str
+
+
 def make_app():
-    # In-memory storage for the API key
-    api_key_storage = {}
-
-    class AppQuery(BaseModel):
-        query: str
-
-    class APIKey(BaseModel):
-        key: str
+    # initalize chain
+    chat_model = ChatOpenAI(temperature=0, model="gpt-4o-mini-2024-07-18")
 
     app = FastAPI()
 
@@ -22,14 +20,6 @@ def make_app():
     async def get_health():
         """health check endpoint"""
         return "ok"
-
-    @app.post("/set_api_key")
-    async def set_api_key(api_key: APIKey):
-        """set api key for chat model endpoint"""
-        if not api_key.key:
-            raise HTTPException(status_code=400, detail="API key is required")
-        api_key_storage["api_key"] = api_key.key
-        return {"message": "API key sets up successfully"}
 
     @app.post("/get_travel_advice")
     async def get_travel_advice(query: AppQuery):
@@ -40,23 +30,14 @@ def make_app():
         Returns:
             dict: A dictionary containing the response from the advice chain.
         Raises:
-            HTTPException: If API key is not set, a HTTPException with status code 500 and detail message "API key for chat model not initialized" is raised.
             HTTPException: If the query is empty, a HTTPException with status code 400 and detail message "Query is required" is raised.
             HTTPException: If an exception occurs during the invocation of the advice chain, a HTTPException with status code 500 and the exception message is raised.
         """
-        if not (api_key := api_key_storage.get("api_key", None)):
-            raise HTTPException(
-                status_code=500, detail="API key for chat model not initialized"
-            )
-        chat_model = ChatOpenAI(
-            temperature=0, model="gpt-4o-mini-2024-07-18", api_key=api_key
-        )
-
         user_query = query.query
         if not user_query:
             raise HTTPException(status_code=400, detail="Query is required")
         if detect_injection(user_query):
-            return {"response": "Plase don't try to inject commands."}
+            raise HTTPException(status_code=400, detail="Injection commands detected.")
         try:
             query2advice_chain = construct_query2advice_chain(chat_model)
             response = query2advice_chain.invoke({"query": user_query})
